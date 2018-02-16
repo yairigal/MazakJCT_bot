c42fa0cadfd8eb26e4171569d5631d7332184569
Yair Yigal <yigalyairn@gmail.com>
2018-02-16 14:54:40 +0200

    added webhook support

--- echobot2.py
+++ echobot2.py
@@ -348,8 +348,9 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
-    updater.start_webhook()
+    updater.start_webhook(listen='127.0.0.1', port=5000, url_path='TOKEN1')
+    updater.bot.set_webhook(webhook_url='https://nmontag.com/bots/'+TOKEN,
+                            certificate=open('/etc/letsencrypt/live/nmontag.com/fullchain.pem', 'rb'))
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
